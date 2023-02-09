8cce103a1872f9f82c7efa22941dcd4f7b144eae
Regan Svoboda <regansvoboda@gmail.com>
2023-02-08 21:38:52 -0600

    final

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 20}px`;
+  }
+});
+
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left + 20}px`;
+  }
+});
